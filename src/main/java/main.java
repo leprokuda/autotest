be596acd00f0aaef114897f78c86c63e284f28fd
leprokuda <leprokuda@gmail.com>
2022-05-26 14:27:54 +0700

    auto login

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,21 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","D:\\Programm\\ChromeDriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://vk.com/leprokuda");
+
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement Username = driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
+        Username.sendKeys("standard_user");
+        WebElement Password = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+        Password.sendKeys("secret_sauce");
+        WebElement Login = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+        Login.click();
+
     }
 }
